cf20e595774eb481d87d35296ec831f97d20776f
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-05-22 19:52:46 -0300

    Foi retirada a maneira de consulta anterior, agora estou utilizando uma interface para realizar a consulta e criação do endpoint por pesquisa de ID de usuários.

--- src/main/java/team/martin/controlador/api/controller/CarrosController.java
+++ src/main/java/team/martin/controlador/api/controller/CarrosController.java
@@ -1,23 +1,35 @@
 package team.martin.controlador.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import team.martin.controlador.domain.repository.UserRepository;
 import team.martin.controlador.model.Usuarios;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CarrosController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/users")
     public List<Usuarios> listar(){
 
-        return manager.createQuery("from usuarios", Usuarios.class).getResultList();
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{userid}")
+    public Usuarios buscarID(@PathVariable Long userid){
+
+        Optional<Usuarios> user = userRepository.findById(userid);
+
+        return user.orElse(null);
     }
 }
