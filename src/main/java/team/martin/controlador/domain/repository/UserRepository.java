018e20362462f60929731f6c93d8518bd99a0b78
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-05-23 00:54:48 -0300

    Foi implementado a Optional para buscar o e-mail e o cpf para assim verificar duplicatas na classe UsuarioCadastroService.

--- src/main/java/team/martin/controlador/domain/repository/UserRepository.java
+++ src/main/java/team/martin/controlador/domain/repository/UserRepository.java
@@ -2,10 +2,14 @@ package team.martin.controlador.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import team.martin.controlador.model.Usuarios;
+import team.martin.controlador.model.Usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<Usuarios, Long> {
+public interface UserRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByCpf(String cpf);
 }
