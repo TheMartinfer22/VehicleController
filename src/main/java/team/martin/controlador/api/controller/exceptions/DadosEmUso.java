14279a4c830c715ccadf6060a6bc7c332d5122b0
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-05-23 00:53:58 -0300

    Foi implementado uma exception para dados em uso.

---
+++ src/main/java/team/martin/controlador/api/controller/exceptions/DadosEmUso.java
@@ -0,0 +1,9 @@
+package team.martin.controlador.api.controller.exceptions;
+
+public class DadosEmUso extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public DadosEmUso(String menesagem){
+        super(menesagem);
+    }
+}
