1c7fb8178140786a37df732c880cc2b0df0fce06
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-05-22 22:52:42 -0300

    Foi implementado uma melhoria em resultados via REST API. Possibilitando frases.

---
+++ src/main/java/team/martin/controlador/api/controller/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,30 @@
+package team.martin.controlador.api.controller.exceptionhandler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Resultado result = new Resultado();
+        result.setStatus(status.value());
+        result.setMotivo("O arqumento que foi digitado não é válido.");
+        result.setTime(LocalDateTime.now());
+
+        return handleExceptionInternal(ex, result, headers, status, request);
+    }
+
+
+
+}
